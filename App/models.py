2740404bd4fe867991ec16f1c9e34d12c7f12350
Nwabueze Franklin <nwafrank2017@gmail.com>
2023-05-22 21:41:02 +0100

    Update models.py

--- App/models.py
+++ App/models.py
@@ -2,6 +2,7 @@ from django.db import models
 
 # Create your models here.
 
+
 class Article(models.Model):
     topic = models.CharField(max_length=100)
     author= models.CharField(max_length=100)
