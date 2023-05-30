2b2f5012830522006c8e8a6aa2ce70f88eeff7d2
Nwabueze Franklin <nwafrank2017@gmail.com>
2023-05-30 20:04:28 +0100

    Update views.py

--- App/views.py
+++ App/views.py
@@ -1,5 +1,6 @@
 
 from django.shortcuts import render, get_object_or_404
+
 from django.http import HttpResponse, JsonResponse
 from  rest_framework.parsers import JSONParser
 from . models import Article, Profile
